6a30d587c4bf6773a040fa8315fb2bbed80972df
françois <fgdemussy@gmail.com>
2021-05-10 23:55:53 -0400

    feat: basic get & create Account

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
+	"strconv"
 
 	"github.com/fgdemussy/luca/data"
 	"github.com/fgdemussy/luca/ledger"
@@ -14,7 +17,7 @@ var accounts []ledger.Account
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/accounts", getAccounts).Methods(http.MethodGet)
-	// r.HandleFunc("/accounts", createAccount).Methods(http.MethodPost)
+	r.HandleFunc("/accounts", createAccount).Methods(http.MethodPost)
 	r.HandleFunc("/accounts/{id}", getAccount).Methods(http.MethodGet)
 	// r.HandleFunc("/accounts/{id}", updateAccount).Methods(http.MethodPut)
 	// r.HandleFunc("/accounts/{id}", deleteAccount).Methods(http.MethodDelete)
@@ -33,9 +36,29 @@ func getAccount(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	for _, item := range accounts {
 		if item.ID == params["id"] {
-			data.ToJSON(item, w)
-			break
+			err := data.ToJSON(item, w)
+			if err != nil {
+				fmt.Println("Unable to marshal JSON:", err)
+			}
+			return
 		}
 	}
 	data.ToJSON(&ledger.Account{}, w)
+}
+
+func createAccount(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "application/json")
+	var account *ledger.Account
+	err := data.FromJSON(&account, r.Body)
+	if err != nil {
+		fmt.Println("Can't decode body.", err)
+		return
+	}
+	account.ID = strconv.Itoa(rand.Intn(1000000))
+	accounts = append(accounts, *account)
+	err = data.ToJSON(*account, w)
+	if err != nil {
+		fmt.Println("Unable to marshal JSON:", err)
+		return
+	}
 }
