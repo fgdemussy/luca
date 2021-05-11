d33699a1535f55de7f8a6cc151f3a70257ce03d7
françois <fgdemussy@gmail.com>
2021-05-10 23:29:16 -0400

    feat: getAccount

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 
@@ -16,7 +15,7 @@ func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/accounts", getAccounts).Methods(http.MethodGet)
 	// r.HandleFunc("/accounts", createAccount).Methods(http.MethodPost)
-	// r.HandleFunc("/accounts/{id}", getAccount).Methods(http.MethodGet)
+	r.HandleFunc("/accounts/{id}", getAccount).Methods(http.MethodGet)
 	// r.HandleFunc("/accounts/{id}", updateAccount).Methods(http.MethodPut)
 	// r.HandleFunc("/accounts/{id}", deleteAccount).Methods(http.MethodDelete)
 
@@ -25,7 +24,18 @@ func main() {
 }
 
 func getAccounts(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("getAccounts")
 	w.Header().Add("Content-Type", "application/json")
 	data.ToJSON(accounts, w)
+}
+
+func getAccount(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for _, item := range accounts {
+		if item.ID == params["id"] {
+			data.ToJSON(item, w)
+			break
+		}
+	}
+	data.ToJSON(&ledger.Account{}, w)
 }
