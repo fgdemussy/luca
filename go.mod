588c842b8090f89b0eef893a1c13b6dce8733edc
françois <fgdemussy@gmail.com>
2021-05-10 23:19:05 -0400

    feat: mux & money packages

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/fgdemussy/luca
 
 go 1.16
 
-require github.com/Rhymond/go-money v1.0.2
+require (
+	github.com/Rhymond/go-money v1.0.2
+	github.com/gorilla/mux v1.8.0 // indirect
+)
--- main.go
+++ main.go
@@ -2,12 +2,30 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 
+	"github.com/fgdemussy/luca/data"
 	"github.com/fgdemussy/luca/ledger"
+	"github.com/gorilla/mux"
 )
 
+var accounts []ledger.Account
+
 func main() {
-	fmt.Println("START")
-	a := ledger.Account{Name: "Hola"}
-	fmt.Println(a.Name)
+	r := mux.NewRouter()
+	r.HandleFunc("/accounts", getAccounts).Methods(http.MethodGet)
+	// r.HandleFunc("/accounts", createAccount).Methods(http.MethodPost)
+	// r.HandleFunc("/accounts/{id}", getAccount).Methods(http.MethodGet)
+	// r.HandleFunc("/accounts/{id}", updateAccount).Methods(http.MethodPut)
+	// r.HandleFunc("/accounts/{id}", deleteAccount).Methods(http.MethodDelete)
+
+	// Bind to a port and pass our router in
+	log.Fatal(http.ListenAndServe(":8000", r))
+}
+
+func getAccounts(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("getAccounts")
+	w.Header().Add("Content-Type", "application/json")
+	data.ToJSON(accounts, w)
 }
